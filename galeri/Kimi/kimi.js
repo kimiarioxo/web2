41eea57b8dcb3c7538ba35ee06d65f67294d1742
Uğur Kağan <ugrkgnwork@gmail.com>
2025-03-15 22:46:22 +0300

    Add files via upload

---
+++ galeri/Kimi/kimi.js
@@ -0,0 +1,23 @@
+function saatim(){
+    var aylar = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+    tarih=new Date();
+    saat=tarih.getHours();
+    dakika=tarih.getMinutes();
+    hdk=saat+":"+dakika;
+    gun=tarih.getDate();
+    ay=aylar[tarih.getMonth()];
+    if(ay-10<0){
+        ay="0"+aylar[tarih.getMonth()];
+    }
+    yil=tarih.getFullYear();
+    yga=gun+":"+ay+":"+yil;
+    document.getElementById("tarih").innerHTML=yga;
+    document.getElementById("saatdakika").innerHTML=hdk;
+}
+function galeri(fotoid,fotorsc){
+    fotolar=document.getElementsByTagName("img").item(fotoid).style.cssText="border:1px solid red; opacity:0.5;"
+    document.getElementById("large").src=fotorsc;
+}
+function galericik(fotoid){
+    fotolar=document.getElementsByTagName("img").item(fotoid).style.cssText="border:0px; opacity:1;"
+}
